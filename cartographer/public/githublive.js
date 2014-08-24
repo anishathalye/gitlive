831c7ba77a38290a508d5e6ab966eb047c34f875
Anish Athalye <me@anishathalye.com>
2014-08-24 11:47:55 -0400

    Clean up code

--- cartographer/public/githublive.js
+++ cartographer/public/githublive.js
@@ -29,7 +29,7 @@ var locs = [];
 
 var processEvent = function(from, to) {
     locs.push({origin: from, destination: to});
-    map.arcc(locs, {strokeWidth: 2});
+    map.linez(locs, {strokeWidth: 2});
 };
 
 var processRawEvent = function(event) {
@@ -54,66 +54,104 @@ source.onmessage = function(e) {
     processRawEvent(event);
 };
 
-var arccConfig = {
-  strokeColor: '#DD1C77',
-  strokeWidth: 1,
-  arcSharpness: 1,
-  animationSpeed: 600
-}
-          var bubbleOpts =  {
-        borderWidth: 2,
-        borderColor: '#FFFFFF',
-        popupOnHover: true,
-        popupTemplate: function(geography, data) {
-          return '<div class="hoverinfo"><strong>' + data.name + '</strong></div>';
-        },
-        fillOpacity: 0.75,
-        animate: true,
-        highlightOnHover: true,
-        highlightFillColor: '#0000FF',
-        highlightBorderColor: 'rgba(0, 0, 255, 0.2)',
-        highlightBorderWidth: 2,
-        highlightFillOpacity: 0.85,
-        exitDelay: 100
-    };
+var linezOptions = {
+    strokeColor: '#000000',
+    fillColor: '#111111',
+    strokeWidth: 1,
+    arcSharpness: 1,
+    borderWidth: 2,
+    highlightBorderWidth: 3,
+    borderColor: '#FFFFFF',
+    popupTemplate: function(geography, data) {
+        // remember to escape data
+        return '<div class="hoverinfo">' + '<strong>this</strong> <em>is</em> a test' + '</div>';
+    },
+    fillOpacity: 0.75,
+    highlightFillOpacity: 0.9
+};
 
-  function handleArcc (layer, data, options) {
-    var self = this,
-        svg = this.svg;
+var handleLinez = function(layer, data) {
+    var self = this;
+    var svg = this.svg;
 
-    if ( !data || (data && !data.slice) ) {
-      throw "Datamaps Error - arcs must be an array";
-    }
+    var linez = layer.selectAll('path.githublive-linez').data(data, JSON.stringify);
+    console.log(linez);
 
-    if ( typeof options === "undefined" || true ) {
-      options = arccConfig;
-    }
+    linez.enter().append('svg:circle')
+        .attr('class', 'githublive-linez')
+        .attr('cx', function(datum) {
+            var latLng = self.latLngToXY(datum.origin.latitude, datum.origin.longitude);
+            if (latLng) {
+                return latLng[0];
+            }
+        })
+        .attr('cy', function(datum) {
+            var latLng = self.latLngToXY(datum.origin.latitude, datum.origin.longitude);
+            if (latLng) {
+                return latLng[1];
+            }
+        })
+        .attr('r', 0)
+        .attr('data-info', function(d) {
+            return JSON.stringify(d);
+        })
+        .style('stroke', linezOptions.borderColor)
+        .style('stroke-width', linezOptions.borderWidth)
+        .style('fill-opacity', linezOptions.fillOpacity)
+        .style('fill', function(datum) {
+            if (datum.options && datum.options.fillColor) {
+                return datum.options.fillColor;
+            }
+            return linezOptions.fillColor;
+        })
+        .on('mouseover', function(datum) {
+            var $this = d3.select(this);
 
-    var arcs = layer.selectAll('path.datamaps-arc').data( data, JSON.stringify );
+            var previousAttributes = {
+                'fill': $this.style('fill'),
+                'stroke': $this.style('stroke'),
+                'stroke-width': $this.style('stroke-width'),
+                'fill-opacity': $this.style('fill-opacity')
+            };
 
-    arcs
-      .enter()
-        .append('svg:path')
-        .attr('class', 'datamaps-arc')
+            $this
+                .style('fill', linezOptions.highlightFillOpacity)
+                .style('stroke-width', linezOptions.highlightBorderWidth)
+                .attr('data-previousAttributes', JSON.stringify(previousAttributes));
+
+            self.updatePopup($this, datum, linezOptions, svg);
+        })
+        .on('mouseout', function(datum) {
+            var $this = d3.select(this);
+            
+            var previousAttributes = JSON.parse($this.attr('data-previousAttributes'));
+            for (var attr in previousAttributes) {
+                $this.style(attr, previousAttributes[attr]);
+            }
+
+            d3.selectAll('.datamaps-hoverover').style('display', 'none');
+        })
+        .transition().delay(0).duration(400)
+            .attr('r', 10)
+        .transition().delay(400).duration(200)
+            .attr('r', 5);
+
+    linez.enter().append('svg:path')
+        .attr('class', 'githublive-linez')
         .style('stroke-linecap', 'round')
         .style('stroke', function(datum) {
-          if ( datum.options && datum.options.strokeColor) {
-            return datum.options.strokeColor;
-          }
-          return  options.strokeColor
+            if (datum.options && datum.options.strokeColor) {
+                return datum.options.strokeColor;
+            }
+            return linezOptions.strokeColor;
         })
         .style('fill', 'none')
-        .style('stroke-width', function(datum) {
-          if ( datum.options && datum.options.strokeWidth) {
-            return datum.options.strokeWidth;
-          }
-          return options.strokeWidth;
-        })
+        .style('stroke-width', linezOptions.strokeWidth)
         .attr('d', function(datum) {
             var originXY = self.latLngToXY(datum.origin.latitude, datum.origin.longitude);
             var destXY = self.latLngToXY(datum.destination.latitude, datum.destination.longitude);
             var midXY = [ (originXY[0] + destXY[0]) / 2, (originXY[1] + destXY[1]) / 2];
-            return "M" + originXY[0] + ',' + originXY[1] + "S" + (midXY[0] + (50 * options.arcSharpness)) + "," + (midXY[1] - (75 * options.arcSharpness)) + "," + destXY[0] + "," + destXY[1];
+            return "M" + originXY[0] + ',' + originXY[1] + "S" + (midXY[0] + (50 * linezOptions.arcSharpness)) + "," + (midXY[1] - (75 * linezOptions.arcSharpness)) + "," + destXY[0] + "," + destXY[1];
         })
         .attr('stroke-dasharray', function() {
             var length = this.getTotalLength();
@@ -123,188 +161,75 @@ var arccConfig = {
             var length = this.getTotalLength();
             return length;
         })
-        .transition().delay(400)
-            .duration(600).ease('linear').attr('stroke-dashoffset', 0)
+        .transition()
+            .delay(400)
+            .duration(600)
+            .ease('linear')
+            .attr('stroke-dashoffset', 0);
 
-    arcs
-      .enter()
-        .append('svg:circle')
-        .attr('class', 'datamaps-bubble')
-        .attr('cx', function ( datum ) {
-          var latLng;
-          if ( datumHasCoords(datum.destination) ) {
-            latLng = self.latLngToXY(datum.destination.latitude, datum.destination.longitude);
-          }
-          else if ( datum.centered ) {
-            latLng = self.path.centroid(svg.select('path.' + datum.centered).data()[0]);
-          }
-          if ( latLng ) return latLng[0];
+    linez.enter().append('svg:circle')
+        .attr('class', 'githublive-linez')
+        .attr('cx', function(datum) {
+            var latLng = self.latLngToXY(datum.destination.latitude, datum.destination.longitude);
+            if (latLng) {
+                return latLng[0];
+            }
         })
-        .attr('cy', function ( datum ) {
-          var latLng;
-          if ( datumHasCoords(datum.destination) ) {
-            latLng = self.latLngToXY(datum.destination.latitude, datum.destination.longitude);
-          }
-          else if ( datum.centered ) {
-            latLng = self.path.centroid(svg.select('path.' + datum.centered).data()[0]);
-          }
-          if ( latLng ) return latLng[1];;
+        .attr('cy', function(datum) {
+            var latLng = self.latLngToXY(datum.destination.latitude, datum.destination.longitude);
+            if (latLng) {
+                return latLng[1];
+            }
         })
-        .attr('r', 0) //for animation purposes
+        .attr('r', 0)
         .attr('data-info', function(d) {
-          return JSON.stringify(d);
+            return JSON.stringify(d);
         })
-        .style('stroke', function ( datum ) {
-          return typeof datum.borderColor !== 'undefined' ? datum.borderColor : bubbleOpts.borderColor;
+        .style('stroke', linezOptions.borderColor)
+        .style('stroke-width', linezOptions.borderWidth)
+        .style('fill-opacity', linezOptions.fillOpacity)
+        .style('fill', function(datum) {
+            if (datum.options && datum.options.fillColor) {
+                return datum.options.fillColor;
+            }
+            return linezOptions.fillColor;
         })
-        .style('stroke-width', function ( datum ) {
-          return typeof datum.borderWidth !== 'undefined' ? datum.borderWidth : bubbleOpts.borderWidth;
-        })
-        .style('fill-opacity', function ( datum ) {
-          return typeof datum.fillOpacity !== 'undefined' ? datum.fillOpacity : bubbleOpts.fillOpacity;
-        })
-        .style('fill', function ( datum ) {
-          return '#0000FF';
-        })
-        .on('mouseover', function ( datum ) {
-          var $this = d3.select(this);
+        .on('mouseover', function(datum) {
+            var $this = d3.select(this);
 
-          if (bubbleOpts.highlightOnHover) {
-            //save all previous attributes for mouseout
             var previousAttributes = {
-              'fill':  $this.style('fill'),
-              'stroke': $this.style('stroke'),
-              'stroke-width': $this.style('stroke-width'),
-              'fill-opacity': $this.style('fill-opacity')
+                'fill': $this.style('fill'),
+                'stroke': $this.style('stroke'),
+                'stroke-width': $this.style('stroke-width'),
+                'fill-opacity': $this.style('fill-opacity')
             };
 
             $this
-              .style('fill', bubbleOpts.highlightFillColor)
-              .style('stroke', bubbleOpts.highlightBorderColor)
-              .style('stroke-width', bubbleOpts.highlightBorderWidth)
-              .style('fill-opacity', bubbleOpts.highlightFillOpacity)
-              .attr('data-previousAttributes', JSON.stringify(previousAttributes));
-          }
+                .style('fill', linezOptions.highlightFillOpacity)
+                .style('stroke-width', linezOptions.highlightBorderWidth)
+                .attr('data-previousAttributes', JSON.stringify(previousAttributes));
 
-          if (bubbleOpts.popupOnHover) {
-            self.updatePopup($this, datum, bubbleOpts, svg);
-          }
+            self.updatePopup($this, datum, linezOptions, svg);
         })
-        .on('mouseout', function ( datum ) {
-          var $this = d3.select(this);
-
-          if (bubbleOpts.highlightOnHover) {
-            //reapply previous attributes
-            var previousAttributes = JSON.parse( $this.attr('data-previousAttributes') );
-            for ( var attr in previousAttributes ) {
-              $this.style(attr, previousAttributes[attr]);
+        .on('mouseout', function(datum) {
+            var $this = d3.select(this);
+            
+            var previousAttributes = JSON.parse($this.attr('data-previousAttributes'));
+            for (var attr in previousAttributes) {
+                $this.style(attr, previousAttributes[attr]);
             }
-          }
 
-          d3.selectAll('.datamaps-hoverover').style('display', 'none');
+            d3.selectAll('.datamaps-hoverover').style('display', 'none');
         })
         .transition().delay(1000).duration(400)
-          .attr('r', function ( datum ) {
-              console.log(arguments);
-            // return datum.radius;
-            return 10;
-          })
+            .attr('r', 10)
         .transition().delay(1400).duration(200)
-          .attr('r', 5)
+            .attr('r', 5);
 
-    arcs
-      .enter()
-        .append('svg:circle')
-        .attr('class', 'datamaps-bubble')
-        .attr('cx', function ( datum ) {
-          var latLng;
-          if ( datumHasCoords(datum.origin) ) {
-            latLng = self.latLngToXY(datum.origin.latitude, datum.origin.longitude);
-          }
-          else if ( datum.centered ) {
-            latLng = self.path.centroid(svg.select('path.' + datum.centered).data()[0]);
-          }
-          if ( latLng ) return latLng[0];
-        })
-        .attr('cy', function ( datum ) {
-          var latLng;
-          if ( datumHasCoords(datum.origin) ) {
-            latLng = self.latLngToXY(datum.origin.latitude, datum.origin.longitude);
-          }
-          else if ( datum.centered ) {
-            latLng = self.path.centroid(svg.select('path.' + datum.centered).data()[0]);
-          }
-          if ( latLng ) return latLng[1];;
-        })
-        .attr('r', 0) //for animation purposes
-        .attr('data-info', function(d) {
-          return JSON.stringify(d);
-        })
-        .style('stroke', function ( datum ) {
-          return typeof datum.borderColor !== 'undefined' ? datum.borderColor : bubbleOpts.borderColor;
-        })
-        .style('stroke-width', function ( datum ) {
-          return typeof datum.borderWidth !== 'undefined' ? datum.borderWidth : bubbleOpts.borderWidth;
-        })
-        .style('fill-opacity', function ( datum ) {
-          return typeof datum.fillOpacity !== 'undefined' ? datum.fillOpacity : bubbleOpts.fillOpacity;
-        })
-        .style('fill', function ( datum ) {
-          return '#0000FF';
-        })
-        .on('mouseover', function ( datum ) {
-          var $this = d3.select(this);
+    linez.exit()
+        .transition()
+        .style('opacity', 0)
+        .remove();
+};
 
-          if (bubbleOpts.highlightOnHover) {
-            //save all previous attributes for mouseout
-            var previousAttributes = {
-              'fill':  $this.style('fill'),
-              'stroke': $this.style('stroke'),
-              'stroke-width': $this.style('stroke-width'),
-              'fill-opacity': $this.style('fill-opacity')
-            };
-
-            $this
-              .style('fill', bubbleOpts.highlightFillColor)
-              .style('stroke', bubbleOpts.highlightBorderColor)
-              .style('stroke-width', bubbleOpts.highlightBorderWidth)
-              .style('fill-opacity', bubbleOpts.highlightFillOpacity)
-              .attr('data-previousAttributes', JSON.stringify(previousAttributes));
-          }
-
-          if (bubbleOpts.popupOnHover) {
-            self.updatePopup($this, datum, bubbleOpts, svg);
-          }
-        })
-        .on('mouseout', function ( datum ) {
-          var $this = d3.select(this);
-
-          if (bubbleOpts.highlightOnHover) {
-            //reapply previous attributes
-            var previousAttributes = JSON.parse( $this.attr('data-previousAttributes') );
-            for ( var attr in previousAttributes ) {
-              $this.style(attr, previousAttributes[attr]);
-            }
-          }
-
-          d3.selectAll('.datamaps-hoverover').style('display', 'none');
-        })
-        .transition().duration(400)
-          .attr('r', function ( datum ) {
-              console.log(arguments);
-            // return datum.radius;
-            return 10;
-          })
-        .transition().delay(400).duration(200)
-          .attr('r', 5)
-
-    arcs.exit()
-      .transition()
-      .style('opacity', 0)
-      .remove();
-  }
-    function datumHasCoords (datum) {
-      return typeof datum !== 'undefined' && typeof datum.latitude !== 'undefined' && typeof datum.longitude !== 'undefined';
-    }
-
-  map.addPlugin('arcc', handleArcc);
+map.addPlugin('linez', handleLinez);
